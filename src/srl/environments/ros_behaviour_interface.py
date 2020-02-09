ea3075ec51770613ccb19a51b370e85855d4ae83
Gordon <blah>
2020-02-09 13:16:48 +0000

    wip - starting to try to get tuning of yaw PID parameters using GA

--- src/srl/environments/ros_behaviour_interface.py
+++ src/srl/environments/ros_behaviour_interface.py
@@ -1,18 +1,22 @@
+import roslib;
 
-import roslib;roslib.load_manifest("ros_simple_rl")
+roslib.load_manifest("ros_simple_rl")
 import rospy
 from srv_msgs.srv import DisableOutput, ChangeParam, DoHover, LogNav
 from std_srvs.srv import Empty, EmptyResponse
 from std_msgs.msg import Bool
 from vehicle_interface.msg import Vector6, PilotRequest
+from vehicle_interface.srv import BooleanService, BooleanServiceRequest
 from nav_class import Nav
 import random
 import numpy as np
+
 
 class ROSBehaviourInterface(Nav):
     """ Class that encapsulates the ROS services needed to use the Nessie platform in a RL environment using the
     behavioural architecture given by 'roslaunch behaviours behaviours_thruster_underactuated.launch'
     """
+
     def __init__(self):
         self.ManualEndRun = False
         # Services
@@ -21,20 +25,21 @@ class ROSBehaviourInterface(Nav):
         self.yaw_ros_action = rospy.ServiceProxy('/behaviours/yaw/params', ChangeParam)
         self.nav_reset = rospy.ServiceProxy("/nav/reset", Empty)
         self.logNav = rospy.ServiceProxy("/nav/log_nav", LogNav)
+        self.disable_pilot_srv = rospy.ServiceProxy('/pilot/switch', BooleanService)
 
         terminate_run_sub = rospy.Subscriber("/terminate_run", Bool, self._terminateRunCallback)
-        
+
         self.pilot_position_request = rospy.Publisher("/pilot/position_req", PilotRequest)
 
         Nav.__init__(self)
         self.position = self._nav.position
         self.orientation = self._nav.orientation
         self.orientation_rate = self._nav.orientation_rate
-        
+
     def pilotPublishPositionRequest(self, pose):
-		request = PilotRequest()
-		request.position = pose
-		self.pilot_position_request.publish(request)
+        request = PilotRequest()
+        request.position = pose
+        self.pilot_position_request.publish(request)
 
     def _terminateRunCallback(self, msg):
         self.ManualEndRun = msg.data
@@ -44,21 +49,27 @@ class ROSBehaviourInterface(Nav):
         :param req_list: 6 element list which is used in the Vector6 msg type of the pilot request
         :return: nothing
         """
-        assert type(position_request_as_list) == list, "ROSBehaviourInterface.pilot -> request type must be list of 6 length"
+        assert type(
+            position_request_as_list) == list, "ROSBehaviourInterface.pilot -> request type must be list of 6 length"
         self.pilot_srv(Vector6(values=position_request_as_list))
 
     def performAction(self, param_name, param_value, reverse_flag=False, offset=1.0):
         self.yaw_ros_action(param_name, param_value, reverse_flag, offset)
-    
+
     def reset(self):
         self.disable_behaviours(True)
         # while (np.abs(self.orientation.yaw - 1.57) > 0.1)
-        start_yaw = 1.57	# random.randrange(-30.0, 30.0, 5) / 10.0
+        start_yaw = 1.57  # random.randrange(-30.0, 30.0, 5) / 10.0
         self.pilot([0, 0, 0, 0, 0, start_yaw])
         rospy.sleep(2)
         #    print("Sending to start position")
         self.disable_behaviours(False)
-    
+
+    def enable_pilot(self, req):
+        srv_req = BooleanServiceRequest()
+        srv_req.request = req
+        self.disable_pilot_srv(srv_req)
+
     def getReward(self, state, action):
         angle = abs(state["angle"])
         angle_dt = state["angle_deriv"]
---
+++ src/utilities/optimal_control_response.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+import numpy as np
+
+
+def optimal_control_response():
+    response = np.zeros([350, 2])
+    for step in range(350):
+        response[step, :] = [step, func(step)]
+    return response
+
+def func(x):
+    amplitude = np.pi / 2.0
+    y_offset = 0  # -(np.pi / 2.0)
+    result = (amplitude * (1 / (1 + np.exp(-0.07 * (x -70))))) + y_offset
+    return result
